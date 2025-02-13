c8c69eb5b0a8df4aa137a29998629893247d772a
Guilherme Cadima <guilherme.cadima@digisystem.com.br>
2025-02-12 21:26:27 -0300

    correção de erros

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -29,38 +29,29 @@ const experiences = [
 const Experience = () => {
   return (
     <div className='p-8 max-w-[600px] mx-auto'>
-        <h1 className='text-4xl text-gray-200 font-bold text-center mb-12'>Experiência</h1>
-        <motion.div
+      <h1 className='text-4xl text-gray-200 font-bold text-center mb-12'>Experiência</h1>
+      <motion.div
         className='space-y-8'
         initial="hidden"
         animate="visible"
-        >
-            {experiences.map((experience, index) => (
-                // eslint-disable-next-line react/jsx-key
-                <Reveal>
-                <motion.div
-                    key={index}
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: false, amount: 0.3}}
-                    transition={{ duration: 1}}
-                    className=' border border-purple-600 p-6 rounded-lg shadow-md
-                    hover:shadow-xl transition-shadow duration-300 bg-purple-700/10'
-                >
-                    <h2 className='text-gray-100 text-2xl font-semibold'>{experience.company}</h2>
-                    <p className='text-gray-300'>{experience.period}</p>
-                    <p className='text-gray-400 mt-4'>{experience.description}</p>
-
-
-                </motion.div>
-                </Reveal>
-            ))}
-
-        </motion.div>
-        
-
+      >
+        {experiences.map((experience, index) => (
+          <Reveal key={index}> {/* A key foi movida para o componente Reveal */}
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: false, amount: 0.3 }}
+              transition={{ duration: 1 }}
+              className='border border-purple-600 p-6 rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300 bg-purple-700/10'
+            >
+              <h2 className='text-gray-100 text-2xl font-semibold'>{experience.company}</h2>
+              <p className='text-gray-300'>{experience.period}</p>
+              <p className='text-gray-400 mt-4'>{experience.description}</p>
+            </motion.div>
+          </Reveal>
+        ))}
+      </motion.div>
     </div>
-  )
-}
-
+  );
+};
 export default Experience
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -25,46 +25,44 @@ const projects = [
     //   },
     // },
   ]
-const Portfolio = () => {
-  return (
-    <div className='max-w-[1000px] mx-auto p-6 md:my-20' id="portfolio">
+  const Portfolio = () => {
+    return (
+      <div className='max-w-[1000px] mx-auto p-6 md:my-20' id="portfolio">
         <h2 className='text-3xl font-bold text-gray-200 mb-8'>Portfólio</h2>
         {projects.map((project, index) => (
-            <Reveal>
-            <div key={index} 
-            className={`flex flex-col md:flex-row ${index % 2 !== 0 ? 'md:flex-row-reverse' : ''} mb-12`}>
-                <div className='w-full md:w-1/2 p-4'>
-                    <img
-                        src={project.img}
-                        alt={project.title}
-                        className='w-full h-full object-cover rounded-lg shadow-lg'
-                    />
+          <Reveal key={index}> {/* A key foi movida para o componente Reveal */}
+            <div
+              className={`flex flex-col md:flex-row ${index % 2 !== 0 ? 'md:flex-row-reverse' : ''} mb-12`}
+            >
+              <div className='w-full md:w-1/2 p-4'>
+                <img
+                  src={project.img}
+                  alt={project.title}
+                  className='w-full h-full object-cover rounded-lg shadow-lg'
+                />
+              </div>
+              <div className='w-full md:w-1/2 p-4 flex flex-col justify-center'>
+                <h3 className='text-2xl font-semibold text-gray-200 mb-4'>{project.title}</h3>
+                <p className='text-gray-300 mb-4'>{project.description}</p>
+                <div className='flex space-x-4'>
+                  {/* <a href={project.links.site}
+                      className='px-4 py-2 bg-slate-600 text-gray-200 rounded-lg hover:bg-slate-700
+                                  transition duration-300'>
+                      View Site
+                  </a> */}
+                  <a
+                    href={project.links.github}
+                    className='px-4 py-2 bg-slate-600 text-gray-200 rounded-lg hover:bg-slate-700 transition duration-300'
+                  >
+                    <AiOutlineGithub />
+                  </a>
                 </div>
-                <div className='w-full md:w-1/2 p-4 flex flex-col justify-center'>
-                    <h3 className='text-2xl font-semibold text-gray-200 mb-4'>{project.title}</h3>
-                    <p className='text-gray-300 mb-4'>{project.description}</p>
-                    <div className='flex space-x-4'>
-                        {/* <a href={project.links.site}
-                            className='px-4 py-2 bg-slate-600 text-gray-200 rounded-lg hover:bg-slate-700
-                                        transition duration-300'>
-                            View Site
-                        </a> */}
-                        <a href={project.links.github}
-                            className='px-4 py-2 bg-slate-600 text-gray-200 rounded-lg hover:bg-slate-700
-                                        transition duration-300'>
-                            <AiOutlineGithub/>
-                        </a>
-
-                    </div>
-
-                </div>
-
+              </div>
             </div>
-            </Reveal>
+          </Reveal>
         ))}
-        
-    </div>
-  )
-}
+      </div>
+    );
+  };
 
 export default Portfolio
